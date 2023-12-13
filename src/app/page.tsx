e38a91ea78e37e711b4bee5164e74175de927da2
amanuel <amanuel.tadesse@soraunion.com>
2023-12-13 14:33:11 +0300

    🚀 Add search component and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from 'next/image'
+import Search from "@/components/search/Search";
+import Image from "next/image";
 
 export default function Home() {
   return (
-  <div>Home</div> 
-  )
+    <main className="flex items-center justify-center h-screen w-full px-2 sm:px-24">
+      <div className="flex flex-col w-full">
+        <div className="text-white text-xl sm:text-2xl font-bold leading-9 tracking-wide w-full max-md:max-w-full text-center md:text-left">
+          Book Your Dream Venue Today: Find, Reserve, and Play with Ease!
+        </div>
+        <Search />
+      </div>
+    </main>
+  );
 }
