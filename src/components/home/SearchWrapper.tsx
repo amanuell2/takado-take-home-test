98c91f4c7b888ff540675ffdb3c24c0e577837eb
amanuel <amanuel.tadesse@soraunion.com>
2023-12-14 17:50:16 +0300

    🧹 Refactor SearchWrapper component and add AutosuggestPanel

--- src/components/home/SearchWrapper.tsx
+++ src/components/home/SearchWrapper.tsx
@@ -1,28 +1,51 @@
-'use client';
-import React from 'react'
-import { Search } from '@/components/shared/search';
+"use client";
+import React, { use, useState } from "react";
+import { Search } from "@/components/shared/search";
 
+import { Suspense } from "react";
+import AutosuggestPanel from "../shared/panel/AutosuggestPanel";
 
- export const SearchWrapper = async () => {
-
+export const SearchWrapper = () => {
+  const [searchTerms, setSearchTerms] = useState<string>("");
 
   const onSearchChanged = (searchTerms: string) => {
-    console.log("searchTerms", searchTerms);
-  }
+    setSearchTerms(searchTerms);
+  };
 
   const onSearchClicked = (searchTerms: string) => {
-    // call api
-    console.log("searchTerms", searchTerms);
-  }
-  
-  return (
-    <div>
-       <Search 
-          onSearchChanged={onSearchChanged}
-          onSearchClicked={onSearchClicked}
-          additionalClasses="mt-6"
-        />
-    </div>
-  )
-}
+    setSearchTerms(searchTerms);
+  };
 
+  return (
+    <div className="relative">
+      <Search
+        onSearchChanged={onSearchChanged}
+        onSearchClicked={onSearchClicked}
+        additionalClasses="mt-6"
+      />
+      <Suspense fallback={searchTerms.length && <LoadingSuggestions />}>
+        <AutosuggestPanel query={searchTerms} />
+      </Suspense>
+    </div>
+  );
+};
+
+const LoadingSuggestions = () => {
+  return (
+    <div className="absolute top-full mt-2 w-[51rem] bg-white rounded-tl-2xl shadow-lg z-20">
+      <div className="py-2">
+        {Array.from({ length: 5 }, (_, i) => (
+          <div
+            key={i}
+            className="flex items-center justify-between px-6 py-2.5 hover:bg-gray-100 cursor-pointer"
+          >
+            <div className="flex items-center gap-3 flex-1">
+              <div className="w-10 h-10 bg-gray-200 rounded-full"></div>
+              <div className="h-5 bg-gray-200 rounded-full w-[50%]"></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
