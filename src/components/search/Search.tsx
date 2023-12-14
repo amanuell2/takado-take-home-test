e1eb0d6d0695ae6256031044f71ea29482e9e912
amanuel <amanuel.tadesse@soraunion.com>
2023-12-14 13:50:06 +0300

    🚀 Refactor search component to include debounce and throttle functionality

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,20 +1,77 @@
+"use client";
+import { debounce, throttle } from "lodash";
 import Image from "next/image";
 import * as React from "react";
+import { useCallback } from "react";
 
-const Search = () => {
+interface SearchProps {
+  additionalClasses?: string;
+  onSearchClicked?: (searchTerms: string) => void;
+  onSearchChanged?: (searchTerms: string) => void;
+}
+
+const Search = (props: SearchProps) => {
+  const searchTermsRef = React.useRef<string>("");
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const { onSearchClicked, onSearchChanged, additionalClasses } = props;
+
+  const debouncedSearch = useCallback(
+    debounce((nextValue: string) => {
+      try {
+        if (onSearchChanged) {
+          onSearchChanged(nextValue);
+        }
+      } catch (error) {
+        console.log("Error on typing search", error);
+      }
+    }, 300),
+    [onSearchChanged]
+  );
+
+  const throttledOnSearchClicked = useCallback(
+    throttle(
+      () => {
+        try {
+          if (onSearchClicked) {
+            onSearchClicked(searchTermsRef.current);
+          }
+        } catch (error) {
+          console.log("Error on submitting search ", error);
+        }
+      },
+      1000,
+      { trailing: false }
+    ),
+    []
+  );
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    searchTermsRef.current = event.target.value;
+    debouncedSearch(searchTermsRef.current);
+  };
+
   return (
-    <div className="items-stretch flex flex-col md:flex-row w-full justify-between gap-5 mt-6 max-md:max-w-full max-md:flex-wrap">
+    <div
+      className={`items-stretch flex flex-col md:flex-row w-full justify-between gap-5 mt-6 max-md:max-w-full max-md:flex-wrap ${additionalClasses}`}
+    >
       <div className="items-stretch flex flex-col sm:flex-row gap-2 sm:gap-0 rounded-2xl max-md:max-w-full max-md:flex-wrap max-md:justify-center flex-1">
         <div className="items-stretch bg-zinc-700 flex justify-between gap-3 pl-6 pr-20 py-3.5 max-md:max-w-full flex-1 max-md:flex-wrap max-md:px-5 rounded-tl-2xl">
           <Image
             width={24}
             height={24}
             src="/assets/icons/search.svg"
-            alt="Search"
+            alt="search"
           />
-          <div className="text-primary-100  text-base font-regular tracking-wide self-center grow whitespace-nowrap my-auto font-lato">
-            Venue type
-          </div>
+          <label htmlFor="venueType" />
+          <input
+            id="venueType"
+            type="text"
+            ref={inputRef}
+            onChange={handleInputChange}
+            placeholder="Venue type"
+            className="placeholder:text-primary-100 text-white text-base font-regular tracking-wide self-center grow whitespace-nowrap my-auto font-lato bg-transparent border-none focus:outline-none"
+          />
         </div>
         <div className="justify-between items-center bg-zinc-700 flex gap-5 px-6 py-3.5 max-md:px-5">
           <div className="text-primary-100  text-base font-regular tracking-wide my-auto font-lato">
@@ -24,7 +81,7 @@ const Search = () => {
             width={24}
             height={24}
             src="/assets/icons/arrow-down.svg"
-            alt="Search"
+            alt="arrow-down"
           />
         </div>
         <div className="justify-between items-center bg-zinc-700 flex gap-5 px-6 py-3.5 rounded-br-2xl max-md:px-5">
@@ -35,13 +92,17 @@ const Search = () => {
             width={24}
             height={24}
             src="/assets/icons/arrow-down.svg"
-            alt="Search"
+            alt="arrow-down"
           />
         </div>
       </div>
-      <div className="text-zinc-900 text-base font-regular tracking-wide whitespace-nowrap justify-center items-center bg-lime-300 px-16 py-4 rounded-tl-2xl rounded-br-2xl max-md:px-5">
+      <button
+        type="button"
+        className="text-zinc-900 text-base font-regular tracking-wide whitespace-nowrap justify-center items-center bg-lime-300 px-16 py-4 rounded-tl-2xl rounded-br-2xl max-md:px-5"
+        onClick={() => throttledOnSearchClicked()}
+      >
         Search
-      </div>
+      </button>
     </div>
   );
 };
