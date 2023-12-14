d467e1ebc07e0ddb2f6f21b49d32c50fc8e5bd8f
amanuel <amanuel.tadesse@soraunion.com>
2023-12-14 20:27:08 +0300

    🧹 Add dynamic export and fix import in SearchWrapper component

--- src/components/home/SearchWrapper.tsx
+++ src/components/home/SearchWrapper.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { use, useState } from "react";
+import React, { useState } from "react";
 import { Search } from "@/components/shared/search";
 
 import { Suspense } from "react";
